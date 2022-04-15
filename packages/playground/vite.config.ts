e79462beb66390a36593b2b75eaaa6bdd967ef7c
canvascat <canvascat@qq.com>
2022-04-16 00:14:24 +0800

    refactor: svg sprite

---
+++ packages/playground/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { createSvgSpritePlugin } from 'vite-svg-sprite'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    createSvgSpritePlugin([
+      {
+        iconDirs: {
+          cwd: resolve(process.cwd(), 'src/assets/icons'),
+          ignore: 'color',
+        },
+        symbolId: '[dir]-[name]',
+        svgoOptions: {
+          plugins: [
+            'removeMetadata',
+            'removeTitle',
+            'removeDesc',
+            { name: 'removeAttrs', params: { attrs: 'fill' } },
+          ],
+        },
+      },
+      {
+        iconDirs: resolve(process.cwd(), 'src/assets/icons/color'),
+        svgoOptions: {
+          plugins: ['removeMetadata', 'removeTitle', 'removeDesc'],
+        },
+      },
+    ]),
+  ],
+  server: {
+    port: 8080,
+    open: true,
+  },
+})
